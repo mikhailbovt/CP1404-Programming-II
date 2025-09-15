0c4a702d036a0e9b7cf522fbf74ff526b844acce
nekosich <bovt.mikhail@mail.ru>
2025-09-15 18:13:53 +0800

    Update validation.py

--- Week 1/Lecture 1/validation.py
+++ Week 1/Lecture 1/validation.py
@@ -2,7 +2,14 @@ name = str(input("Enter your name: ")).upper()
 while name == "":
     name = str(input("Enter your name: ")).upper()
 print(f"Hello {name}!")
-salary = float(input("Enter your salary: "))
+
+while True:
+    try:
+        salary = float(input("Enter your salary: "))
+        break
+    except ValueError:
+        print("Salary must be a number.")
+
 while salary <= 0:
     salary = float(input("Enter your salary: "))
 print(f"Your salary is ${salary:.2f}")
