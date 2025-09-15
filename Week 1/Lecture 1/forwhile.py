c49a3d69a00532e2e4053185c2d1f26e2fe475bc
nekosich <bovt.mikhail@mail.ru>
2025-09-15 17:44:04 +0800

    Create forwhile.py

---
+++ Week 1/Lecture 1/forwhile.py
@@ -0,0 +1,9 @@
+max_number = int(input("Enter the maximum number: "))
+for i in range(1, max_number + 1):
+    print(i)
+
+i = 1
+max_number = int(input("Enter the maximum number: "))
+while i <= max_number:
+    print(i)
+    i += 1
