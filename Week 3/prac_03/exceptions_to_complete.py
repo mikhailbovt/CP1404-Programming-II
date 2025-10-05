cef7e3c961fc9c8301b538777d50f64a0b564bb0
mikhailbovt <bovt.mikhail@mail.ru>
2025-10-05 19:29:22 +0700

    Finished Exceptions To Complete

--- Week 3/prac_03/exceptions_to_complete.py
+++ Week 3/prac_03/exceptions_to_complete.py
@@ -2,5 +2,14 @@
 CP1404/CP5632 - Practical 3
 Mikhail Bovt
 05/10/2025
-String Formatting
+Exceptions To Complete
 """
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True  # Exit loop once valid input is received
+    except ValueError:  # Catch only invalid number inputs
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
