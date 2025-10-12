0d34827eee5df27b9298b4e45c84a5a7c1e8ba10
mikhailbovt <bovt.mikhail@mail.ru>
2025-10-12 15:43:03 +0700

    Basic list operations + Woefully inadequate security checker

---
+++ Week 4/prac_04/list_exercises.py
@@ -0,0 +1,36 @@
+"""
+CP1404/CP5632 - Practical 4
+Mikhail Bovt
+12/10/2025
+Basic list operations + Woefully inadequate security checker
+"""
+
+def main():
+    # Part 1: Basic list operations 
+    numbers = []
+
+    for i in range(5):
+        number = float(input("Number: "))
+        numbers.append(number)
+
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+    # Part 2: Woefully inadequate security checker
+    usernames = [
+        'jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+        'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+        'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob'
+    ]
+
+    username = input("\nEnter your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+
+main()
